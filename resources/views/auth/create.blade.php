bd6a3b71db20c4771c2a98c77213e5d43c386587
ayatarefin <ayatullaharefin@gmail.com>
2024-01-11 17:20:05 +0600

    still dumb i am

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,48 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-.small-card {
-    max-width: 500px;
-    margin: 0 auto;
-}
-</style>
+    <style>
+        .small-card {
+            max-width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
 
-<div class="card mb-4 small-card">
-    <div class="card-header">
-        <h4>Add New User</h4>
+<body>
+
+    <div class="container mt-4">
+        <div class="card mb-4 small-card">
+            <div class="card-header">
+                <h4>Add New User</h4>
+            </div>
+            <div class="card-body">
+                <form id="addNewUser" action="{{ route('users.store') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name" class="form-label">Full Name</label>
+                        <input class="form-control" type="text" name="name" id="name" placeholder="Full Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="form-label">Email</label>
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email" aria-describedby="emailHelp" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="form-label">Set Password</label>
+                        <input class="form-control" type="password" name="password" id="password" placeholder="Set Password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="admin_key" class="form-label">Admin Key</label>
+                        <input class="form-control" type="text" name="admin_key" id="admin_key" placeholder="Admin Key" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="admin_role" class="form-label">User Role</label>
+                        <select class="form-control" name="admin_role" id="admin_role" required>
+                            @foreach ($roles as $row)
+                            <option value="{{ $row->role_name }}">{{ $row->role_name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-success display-add-user">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        <form id="addNewUser" action="{{ route('users.store') }}" method="post">
-            @csrf
-            <div class="mb-3">
-                <label for="name" class="form-label">Full Name</label>
-                <input class="form-control" type="text" name="name" id="name" placeholder="Full Name" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input class="form-control" type="email" name="email" id="email" placeholder="Email" aria-describedby="emailHelp" required>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Set Password</label>
-                <input class="form-control" type="password" name="password" id="password" placeholder="Set Password" required>
-            </div>
-            <div class="mb-3">
-                <label for="admin_key" class="form-label">Admin Key</label>
-                <input class="form-control" type="text" name="admin_key" id="admin_key" placeholder="Admin Key" required>
-            </div>
-            <div class="mb-3">
-                <label for="role" class="form-label">User Role</label>
-                <select class="form-select" name="admin_role" id="admin_role" required>
-                    @foreach ($roles as $row)
-                        <option value="{{ $row->role_name }}">{{ $row->role_name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="text-center">
-                <button type="submit" class="btn btn-success display-add-user">Save</button>
-            </div>
-        </form>
-    </div>
-</div>
 @endsection
